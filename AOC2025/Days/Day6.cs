ce33347ef581f1e1872fe33d222f647e609b1afe
Scheer van der, EAM (Eva) <Eva.van.der.Scheer@rabobank.com>
2025-12-06 22:50:26 +0100

    day 6 part 1

--- AOC2025/Days/Day6.cs
+++ AOC2025/Days/Day6.cs
@@ -4,16 +4,45 @@ namespace AOC2025.Days
     {
         public static void DoPuzzlePart1()
         {
-            var lines = File.ReadAllLines("./Input/Day5Input.txt");
+            var lines = File.ReadAllLines("./Input/Day6Input.txt");
+            var splitLines = new List<string[]>();
+            foreach (var line in lines)
+            {
+                splitLines.Add(line.Split([" "], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+            }
+            long solution = 0;
 
-            var solution = 0;
+            var operators = splitLines.Last();
+            var numbers = splitLines.Take(splitLines.Count - 1);
+
+            for (int i = 0; i < operators.Length; i++)
+            {
+                var op = operators[i];
+                long result = 0;
+                if (op == "*")
+                {
+                    result = 1;
+                }
+                foreach (var num in numbers)
+                {
+                    if (op == "*")
+                    {
+                        result *= int.Parse(num[i]);
+                    }
+                    if (op == "+")
+                    {
+                        result += int.Parse(num[i]);
+                    }
+                }
+                solution += result;
+            }
 
             Console.WriteLine("solution is: " + solution);
         }
 
         public static void DoPuzzlePart2()
         {
-            var lines = File.ReadAllLines("./Input/Day5Input.txt");
+            var lines = File.ReadAllLines("./Input/Day6Input.txt");
 
             long solution = 0;
 
