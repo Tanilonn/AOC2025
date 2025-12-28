47c25bc5fa18f33dbae205ba9affd9e30e813db4
Scheer van der, EAM (Eva) <Eva.van.der.Scheer@rabobank.com>
2025-12-28 19:07:11 +0100

    part 1

--- AOC2025/Days/Day9.cs
+++ AOC2025/Days/Day9.cs
@@ -6,9 +6,28 @@ namespace AOC2025.Days
         {
             var lines = File.ReadAllLines("../../../Input/Day9Input.txt");
 
-            var solution = 0;
+            // loop through all combinations of two red tiles exactly once
+            long biggestSquare = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = i + 1; j < lines.Length; j++)
+                {
+                    var first = lines[i].Split(',');
+                    var second = lines[j].Split(',');
 
-            Console.WriteLine("solution is: " + solution);
+                    // the square that we can make is the differencce between x + 1 multiplied with the difference between y + 1
+                    var difX = Math.Abs(long.Parse(first[0]) - long.Parse(second[0])) + 1;
+                    var difY = Math.Abs(long.Parse(first[1]) - long.Parse(second[1])) + 1;
+                    var squareSize = difX * difY;
+                    if (squareSize > biggestSquare)
+                    {
+                        biggestSquare = squareSize;
+                    }
+                }
+            }
+
+
+            Console.WriteLine("solution is: " + biggestSquare);
         }
 
         public static void DoPuzzlePart2()
